98ce5db965cd0caf46b8d28de25aa98aa3e58578
TadejPolajnar <tadej.polajnar@gmail.com>
2021-12-29 18:59:48 +0100

    feat: Add vitest eslint globals

---
+++ index.js
@@ -0,0 +1,12 @@
+module.exports = {
+    globals: {
+        vi: true,
+        describe: true,
+        it: true,
+        expect: true,
+        beforeEach: true,
+        afterEach: true,
+        beforeAll: true,
+        afterAll: true
+    }
+};
